2fd43a7c2b1813a77dfff0d51aba9f0b37547f31
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-02-12 12:46:21 +0000

    add logging

--- src/IaC.py
+++ src/IaC.py
@@ -4,6 +4,10 @@ import json
 import psycopg2
 from pathlib import Path
 import botocore.exceptions
+import log_config
+
+# logging
+logger = log_config.logger
 
 
 # récupération des paramètres de configuration dans le fichier config.cfg
@@ -45,6 +49,8 @@ class IaC:
                         )
         
         self._myClusterProps = None
+        
+        logger.debug("cration des clients pour accéder aux services AWS")
 
     # création d'une bucket afin de stocker les données 
     def create_bucket(self) : 
@@ -60,7 +66,7 @@ class IaC:
                 print("Le bucket existe déjà et vous en êtes propriétaire.")
             pass
             
-
+        logger.debug(f"création de la bucket") 
             
     def create_cluster(self) : 
         
@@ -69,7 +75,7 @@ class IaC:
         # création d'un role IAM pour accéder à Redshift
         try : 
 
-            print("1.1 Creating a new IAM Role") 
+            logger.debug("création d'un role IAM pour accéder à Redshift") 
             self._iam.create_role(
                 Path='/',
                 RoleName=config.get("DWH", "DWH_IAM_ROLE_NAME"),
@@ -83,15 +89,16 @@ class IaC:
         except Exception as e : 
             pass
                 
-            print("1.2 Attaching Policy")
 
         try : 
             self._iam.attach_role_policy(RoleName=config.get("DWH", "DWH_IAM_ROLE_NAME"),
                                 PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
                                 )
-
-            print("1.3 Get the IAM role ARN")
+            logger.debug("Attaching Policy")
+            
             roleArn = self._iam.get_role(RoleName=config.get("DWH", "DWH_IAM_ROLE_NAME"))['Role']['Arn']
+            logger.debug("Get the IAM role ARN")
+            
             
         except Exception as e : 
             pass
@@ -115,10 +122,10 @@ class IaC:
                 #Roles (for s3 access)
                 IamRoles=[roleArn]  
                 )
+                
+            logger.debug("création du cluster Redshift")
             
         except Exception as e :
-            
-            print('erreur lors de la création du cluster')
             print(e)
             
                 
@@ -135,6 +142,8 @@ class IaC:
             defaultSg = list(vpc.security_groups.all())[0]
             
             print(defaultSg)
+            logger.debug("ouverture du port 5439 pour accéder au cluster") 
+            
             defaultSg.authorize_ingress(
                 GroupName=defaultSg.group_name,
                 CidrIp='0.0.0.0/0',
@@ -164,10 +173,13 @@ class IaC:
 
         cursor = conn.cursor()
         cursor.execute("SELECT version();")
+        
         print('------------------------------------------ test ------------------------------------------')
         print(cursor.fetchone())
+        
         conn.close()
-
+        logger.debug(f" {cursor.fetchone()}")
+        logger.debug("test ok")
 
 if __name__ == '__main__' : 
     
