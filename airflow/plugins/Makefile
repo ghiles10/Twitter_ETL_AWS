8c254f913ab3f8a5c79a4347c9fb088ec7d9a8c3
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-02-16 15:12:50 +0000

    reglage airflow

---
+++ airflow/plugins/Makefile
@@ -0,0 +1,5 @@
+init: 
+	cd airflow && docker-compose up airflow-init
+
+run:
+	cd airflow && docker-compose up
---
+++ airflow/plugins/src/warehouse/data_warehouse_query.py
@@ -0,0 +1,29 @@
+create_tweet_table = """
+CREATE TABLE IF NOT EXISTS ghiles.tweets 
+(
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,
+    text VARCHAR,
+    favorite_count VARCHAR,
+    retweet_count VARCHAR,
+    date_creation DATE,
+    date DATE
+)
+SORTKEY (date_creation);
+"""
+
+create_user_table = """
+CREATE TABLE IF NOT EXISTS ghiles.users
+(
+    "user" TEXT, 
+    PRIMARY KEY ("user"),
+    description VARCHAR,
+    following VARCHAR,
+    followers VARCHAR, 
+    favorite_count VARCHAR,
+    retweet_count VARCHAR,
+    date_creation DATE,
+    date DATE
+)
+SORTKEY ("user");
+"""
+
