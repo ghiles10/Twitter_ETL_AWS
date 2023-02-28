2ad887455d7ff1643b07fe59e727e51ac3e58a3f
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-02-28 19:36:00 +0000

    git ignore config cfg

--- airflow/plugins/src/log_config.py
+++ airflow/plugins/src/log_config.py
@@ -8,7 +8,7 @@ if not os.path.exists("src/log/"):
 file_name = os.path.basename(sys.argv[0]).split(".")[0] 
 
 logger = logging.getLogger(file_name)
-handler = logging.FileHandler(f"src/log/{file_name}.log", mode = 'w')
+handler = logging.FileHandler(f"/src/log/{file_name}.log", mode = 'w')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(module)s  - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
